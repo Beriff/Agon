864b691d472c8a5e43e732a53dd1f1d1a495d99c
Beriff <58127431+Beriff@users.noreply.github.com>
2024-06-24 23:52:33 +0700

    update .gitignore and tsconfig

--- src/htmlcopy.py
+++ src/htmlcopy.py
@@ -1,2 +1,9 @@
-import shutil
-shutil.copytree('./pages/', './compiled/pages/', dirs_exist_ok=True)
+import shutil, os
+
+for f in os.listdir('./compiled/pages/'): #remove all files in ./compiled/pages
+    fpath = os.path.join('./compiled/pages/', f)
+    if os.path.isfile(fpath) or os.path.islink(fpath):
+        os.unlink(fpath)
+    elif os.path.isdir(fpath):
+        shutil.rmtree(fpath)
+shutil.copytree('./pages/', './compiled/pages/', dirs_exist_ok=True) #copy ./pages/ to ./compiled/pages/
